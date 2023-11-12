eb251a6c7c5586ce24a38f78f408eee3a040369c
Long Nguyen-Vu <nve164@gmail.com>
2023-11-12 17:00:13 +0900

    calculate reference embedding and threshold

--- oc_classifier.py
+++ oc_classifier.py
@@ -3,7 +3,6 @@ import argparse
 import librosa
 import torch
 import torch.nn.functional as F
-import torch.optim as optim
 from torch.nn import DataParallel
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader, random_split
@@ -24,7 +23,7 @@ from losses.custom_loss import compactness_loss, descriptiveness_loss, euclidean
 
 
 class ASVDataset(Dataset):
-    def __init__(self, protocol_file, dataset_dir):
+    def __init__(self, protocol_file, dataset_dir, eval=False):
         """
         Protocol file for LA train
             /datab/Dataset/ASVspoof/LA/ASVspoof_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt
@@ -43,24 +42,39 @@ class ASVDataset(Dataset):
             preprocessed (True, optional): extract directly from raw audio files. Defaults to True.
         """
         self.protocol_file = protocol_file
-        self.file_list = []
-        self.label_list = []
         self.dataset_dir = dataset_dir
-
+        self.file_list = []
+        self.label_list = []  
+        self.eval = eval
         # file_list is now the second column of the protocol file
         # label list is now the fifth column of the protocol file
         # read the protocol file
 
-        with open(self.protocol_file, "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                line = line.strip()
-                line = line.split(" ")
-                self.file_list.append(line[1])
-                self.label_list.append(line[4])
-
-        self._length = len(self.file_list)
-   
+        if self.eval:
+            # collect all files
+            # note the difference between eval and train protocol file
+            with open(self.protocol_file, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    line = line.split(" ")
+                    self.file_list.append(line[1])
+                    self.label_list.append(line[5]) # bonafide or spoof
+            self._length = len(self.file_list)
+        else:
+            # collect bona fide list only
+            # for calculating `reference embedding`           
+            with open(self.protocol_file, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    line = line.split(" ")
+                    if line[4] == "bonafide":
+                        self.file_list.append(line[1])
+                        self.label_list.append(line[4]) # bonafide only
+            self._length = len(self.file_list)
+            
+    
     def __len__(self):
         return self._length
 
@@ -70,7 +84,7 @@ class ASVDataset(Dataset):
         audio_file = self.file_list[idx]
         file_path = os.path.join(self.dataset_dir, audio_file + ".flac")
         feature, _ = librosa.load(file_path, sr=None)
-        label = self.label_list[idx]
+        label = [1 if self.label_list[idx] == "spoof" else 0]
 
         # Convert the list of features and labels to tensors
         feature_tensors = torch.tensor(feature, dtype=torch.float32)
@@ -85,33 +99,74 @@ class ASVDataset(Dataset):
         pass
 
 
+def create_reference_embedding(extractor, encoder, dataloader, device):
+    """Create reference embeddings for one-class classifier
+
+    Args:
+        extractor, encoder (nn.Module): pretrained models (e.g., XLSR, SE-ResNet34)
+        dataloader (DataLoader): dataloader for the dataset
+
+    Returns:
+        torch.Tensor: reference embedding
+    """
+    extractor.eval()
+    encoder.eval()
+    total_embeddings = []
+    total_distances = []
+    
+    with torch.no_grad():
+        for _, (data, target) in enumerate(dataloader):
+            data = data.to(device)
+            target = target.to(device)
+            emb = extractor(data)
+            emb = emb.unsqueeze(1)
+            emb = encoder(emb)
+            total_embeddings.append(emb)
+    
+    # reference embedding is the mean of all embeddings
+    reference_embedding = torch.mean(torch.stack(total_embeddings), dim=0)
+    
+    # threshold is the maximum Euclidean distance between the reference embedding and all embeddings
+    for emb in total_embeddings:
+        distance = F.pairwise_distance(reference_embedding, emb, p=2)
+        total_distances.append(distance)
+    threshold = torch.max(torch.stack(total_distances))
+    
+    return reference_embedding, threshold
+
 if __name__== "__main__":
     parser = argparse.ArgumentParser(description='One-class classifier')
-    parser.add_argument('--train_dataset_dir', type=str, default="/datab/Dataset/ASVspoof/LA/ASVspoof2019_LA_train/flac",
+    parser.add_argument('--protocol_file', type=str, default="/datab/Dataset/ASVspoof/LA/ASVspoof_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt",
+                        help='Path to the protocol file')
+    parser.add_argument('--dataset_dir', type=str, default="/datab/Dataset/ASVspoof/LA/ASVspoof2019_LA_train/flac",
                         help='Path to the dataset directory')
-    parser.add_argument('--test_dataset_dir', type=str, default="/datab/Dataset/ASVspoof/LA/ASVspoof2019_LA_eval/flac",
-                        help='Path to the test dataset directory')
+    args = parser.parse_args()
     
     # initialize xlsr and lcnn models
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     ssl = SSLModel(device)
-    lcnn = lcnn_net(asoftmax=False).to(device)
-
+    senet = se_resnet34().to(device)
 
     # load pretrained weights
     ssl.load_state_dict(torch.load("/datac/longnv/occm/ssl_0.pt"))
-    lcnn.load_state_dict(torch.load("/datac/longnv/occm/lcnn_0.pt"))    
-    lccn = DataParallel(lcnn)
+    senet.load_state_dict(torch.load("/datac/longnv/occm/senet34_0.pt"))
+    senet = DataParallel(senet)
     ssl = DataParallel(ssl)
     print("Pretrained weights loaded")
     
-    audio_file = "/datac/longnv/audio_samples/ADD2023_T2_T_00000000.wav"
-    audio_data, _ = librosa.load(audio_file, sr=None)
-    emb = ssl(torch.Tensor(audio_data).unsqueeze(0).to("cuda"))
-    emb = emb.unsqueeze(1)
-    out = lcnn(emb)
-    predicted = torch.argmax(out, dim=1)
-    print(out)
-    print(predicted)
+    # create a reference embedding & find a threshold
+    print("Creating a reference embedding...")
+    train_dataset = ASVDataset(args.protocol_file, args.dataset_dir)
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=0)
+    reference_embedding, threshold = create_reference_embedding(ssl, senet, train_dataloader, device)
+
+    print(f"reference_embedding.shape = {reference_embedding.shape}")
+    print(f"threshold = {threshold}")
+
+    # audio_file = "/datac/longnv/audio_samples/ADD2023_T2_T_00000000.wav"
+    # audio_data, _ = librosa.load(audio_file, sr=None)
+    # emb = ssl(torch.Tensor(audio_data).unsqueeze(0).to("cuda"))
+    # emb = emb.unsqueeze(1)
+    # emb = senet(emb)
+ 
     
